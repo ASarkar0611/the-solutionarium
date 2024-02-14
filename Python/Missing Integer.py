125ca5257a296ba2cc21d65703b891402130745a
Arunita S <114884556+ASarkar0611@users.noreply.github.com>
2024-02-14 12:31:49 -0500

    Create Missing Integer.py

---
+++ Python/Missing Integer.py
@@ -0,0 +1,6 @@
+# Missing Integer [Amazon Python Interview Question]
+
+def missing_int(input: list[int])-> int:
+  list_len = len(input)
+  for i in range(list_len+1):
+    if i not in input: return i
