9d9d20a0990b722bfcebaaefea28b244f1700dbd
Arunita S <114884556+ASarkar0611@users.noreply.github.com>
2024-02-14 12:32:13 -0500

    Update Missing Integer.py

--- Python/Missing Integer.py
+++ Python/Missing Integer.py
@@ -1,4 +1,4 @@
-# Missing Integer [Amazon Python Interview Question]
+# Easy - Missing Integer [Amazon Python Interview Question]
 
 def missing_int(input: list[int])-> int:
   list_len = len(input)
