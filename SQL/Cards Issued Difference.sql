35a9f9a705c85a5086e86f1e2b4cb7ecea73694a
Arunita S <114884556+ASarkar0611@users.noreply.github.com>
2024-02-29 16:00:13 -0500

    Create Cards Issued Difference.sql

---
+++ SQL/Cards Issued Difference.sql
@@ -0,0 +1,6 @@
+-- Cards Issued Difference [JPMorgan Chase SQL Interview Question]
+
+SELECT card_name, (MAX(issued_amount) - MIN(issued_amount)) as difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC;
